f938b59f4b21140332eac1b5a3eb90ac243e1df3
Ramires102 <p.chiavetta@alumno.etec.um.edu.ar>
2025-04-04 12:41:58 -0300

    mas info

--- otro_hola_mundo.py
+++ otro_hola_mundo.py
@@ -2,3 +2,4 @@ print ("xd")
 print ("Hola Mundo")
 #comando para cambiar de ramas = git checkout (nombre_rama)
 #comando para ver las ramas = git branch
+#como crear una rama = git branch (nombre_rama)
